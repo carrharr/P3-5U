95ea56d1aa142d2a51e91b1dd0d178b3764f4f7e
carrharr <danielcarrilloharris@gmail.com>
2016-01-12 10:51:12 +0000

    app w/ products and categories implemented

--- catalog/app/models.py
+++ catalog/app/models.py
@@ -1,5 +1,48 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.utils import timezone
+import datetime
 # Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length=140)
+	slug = models.SlugField(max_length=140, unique=True)
+	description = models.TextField()
+	is_active = models.BooleanField(default=True)
+	meta_keywords = models.CharField("Meta Keywords", max_length=255)
+	meta_description = models.CharField("Meta Description", max_length=255)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+class Meta:
+	db_table = 'categories'
+	ordering = ['-created_at']
+	verbose_name_plural = 'Categories'
+def __unicode__(self):
+	return self.name
+@models.permalink
+def get_absolute_url(self):
+	return ('catalog_category',(), { 'category_slug': self.slug })
+
+# UNFINISHED CLASS PRODUCT
+class Product(models.Model):
+	name = models.CharField(max_length=140)
+	slug = models.SlugField()
+	description = models.CharField(max_length=500)
+	available = models.BooleanField(default=False)
+	picture = models.ImageField(upload_to=None, max_length=140)
+	sku = models.CharField(max_length=140)
+	price = models.DecimalField(max_digits=9, decimal_places=2)
+	is_featured =  models.BooleanField(default=False)
+	meta_keywords = models.CharField("Meta Keywords", max_length=255)
+	meta_description = models.CharField("Meta Description", max_length=255)
+	published_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	categories = models.ManyToManyField(Category)
+class Meta:
+	db_table = 'products'
+	ordering = '-published_at'
+def __unicode__(self):
+	return self.name
+@models.permalink
+def get_absolute_url(self):
+	return ('catalog_product', (), { 'product_slug': self.slug })
--- catalog/catalog/settings.py
+++ catalog/catalog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'catalog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'catalog',
+        'USER': 'catalog',
+        'PASSWORD': 'qwerty',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
